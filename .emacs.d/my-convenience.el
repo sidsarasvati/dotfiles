668ff97a963af803d4dbede0b22b026654f09a5b
Sid Sarasvati <sidsarassvati@gmail.com>
2013-02-28 13:34:17 -0500

    new mode(cmake) specific key binding C-c-h for cmake help :)

--- .emacs.d/my-convenience.el
+++ .emacs.d/my-convenience.el
@@ -8,3 +8,12 @@
 
 (global-set-key "\M-1" 'delete-other-windows)
 (global-set-key "\M-`" 'previous-buffer)
+
+
+;;MODE SPECIFIC
+
+;;semantic 
+;;TODO
+;;cmake
+(eval-after-load 'cmake
+                     '(define-key CMAKE-mode-map (kbd "\C-c-h") 'cmake-help-command))
