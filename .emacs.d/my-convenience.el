f96a749203cd68e2230244d4257042401d1d8cde
Sid Sarasvati <sidsarasvati@gmail.com>
2013-05-18 16:57:38 -0400

    M-k for kill buffer.. don't wanna press two chords for useful funcitons

--- .emacs.d/my-convenience.el
+++ .emacs.d/my-convenience.el
@@ -4,7 +4,7 @@
 (global-set-key "\C-x\C-g" 'find-file)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
-(global-set-key "\C-x\C-k" 'kill-buffer)
+(global-set-key "\M-k" 'kill-buffer)
 
 ;;other useful stuff
 (global-set-key "\M-g" 'grep)
