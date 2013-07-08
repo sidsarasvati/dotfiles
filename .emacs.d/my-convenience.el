8cd00b541b2291129635556ae8cc370add6db7e4
Sid Sarasvati <sidsarasvati@gmail.com>
2013-07-08 13:26:56 -0400

    don't need dup. key bindings to run extended-commands i.e meta

--- .emacs.d/my-convenience.el
+++ .emacs.d/my-convenience.el
@@ -2,12 +2,10 @@
 
 ;;duplicate binding for my convenience
 (global-set-key "\C-x\C-g" 'find-file)
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
 (global-set-key "\M-k" 'kill-buffer)
 
 ;;other useful stuff
-(global-set-key "\M-g" 'grep)
+;;(global-set-key "\M-g" 'grep)
 
 ;;buffer management 
 (global-set-key "\M-1" 'delete-other-windows)
