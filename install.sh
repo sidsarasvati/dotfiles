85649f3be08149bf41503e672228732ff7578177
Sid Sarasvati <sid@cogitocorp.com>
2019-06-12 12:30:18 -0400

    added homebrew and package install as part of setup script

--- install.sh
+++ install.sh
@@ -1,10 +1,47 @@
-# Link other files
-stow bash
-stow git
-stow emacs
-stow misc # all other things that don't fit cleanly in a category
+#!/bin/bash
+
+set -eu
+
+HOMEBREW_DOWNLOAD_URL=https://raw.githubusercontent.com/Homebrew/install/master/install
+
+install_homebrew() {
+  echo "Checking for Homebrew, and installing if necessary"
+  if ! [[ $( command -v brew ) ]]; then
+    echo 'Installing homebrew...'
+    /usr/bin/ruby -e "$(curl -fsSL $HOMEBREW_DOWNLOAD_URL)" > /dev/null
+  else
+    echo 'Homebrew installed.'
+  fi
+}
+
+# TODO - use brew bundle
+install_packages() {
+  echo "Installing packages..." 
+  brew install stow > /dev/null ||  brew upgrade stow > /dev/null
+}
 
 
-# Link .bash_profile -> .bashrc
-rm -f ~/.bash_profile
-ln -s ~/.bashrc ~/.bash_profile
+stow_files() {
+  # Link other files
+  stow bash
+  stow git
+  stow emacs
+  stow misc # all other things that don't fit cleanly in a category
+
+  # Link .bash_profile -> .bashrc
+  rm -f ~/.bash_profile
+  ln -s ~/.bashrc ~/.bash_profile
+}
+
+
+
+main() {
+  install_homebrew
+  install_packages    
+  stow_files
+}
+
+main
+
+echo ""
+echo "SUCCESS. Restart the terminal app for the new config to take effect."
